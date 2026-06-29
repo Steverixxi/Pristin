3bf299e2357c247dba14cea9a6eff7abff045143
Steverixxi <stefanwerner05@gmail.com>
2026-06-29 22:19:55 +0200

    Initial Commit

---
+++ Pristin/PristinApp.swift
@@ -0,0 +1,17 @@
+//
+//  PristinApp.swift
+//  Pristin
+//
+//  Created by Stefan on 29.06.26.
+//
+
+import SwiftUI
+
+@main
+struct PristinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
